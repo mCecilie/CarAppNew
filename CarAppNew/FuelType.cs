63f22ef5ac0e2805a40068991fbd7463bfb41e40
Cecilie <mcmo79137@edu.ucl.dk>
2026-03-16 11:06:49 +0700

    Add project files.

---
+++ CarAppNew/FuelType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarAppNew
+{
+    // Creates an enumeration called FuelType with the following values: Benzin, Diesel, Electric, Hybrid.
+    public enum FuelType
+
+    {
+        Benzin,
+        Diesel,
+        Electric,
+        Hybrid
+    }
+}
---
+++ CarAppNew/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CarAppNew
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
