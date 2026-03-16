815db8a342c518f4ebbb00080186148583719467
Cecilie <mcmo79137@edu.ucl.dk>
2026-03-16 11:34:44 +0700

    Øvelse 3: Opretter main program og indtaster kode

--- CarAppNew/Program.cs
+++ CarAppNew/Program.cs
@@ -1,10 +1,40 @@
 ﻿namespace CarAppNew
 {
-    internal class Program
+    using System;
+    using System.Collections.Generic;
+
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Opret en ny bil
+            Car myCar = new Car("Toyota", "Corolla", 2020, "AB12345", FuelType.Benzin, 22.5);
+            myCar.TurnOnEngine();
+
+            // Opretter ture – alle er fuldt initialiseret via konstruktøren
+            List<Trip> trips = new List<Trip>
+
+// Trip-objekter oprettes med alle nødvendige data, og Car-objektet er en del af konstruktionen
+{
+new Trip(myCar, 50, DateTime.Now, DateTime.Now.AddHours(1)),
+new Trip(myCar, 30, DateTime.Now, DateTime.Now.AddMinutes(30)),
+new Trip(myCar, 100, DateTime.Now, DateTime.Now.AddHours(2))
+};
+
+            // Drive tilføjer én tur ad gangen og tjekker bil-tilhørsforhold
+            foreach (var trip in trips)
+            {
+                myCar.Drive(trip);
+            }
+
+            // Main har ansvar for udskrivningen – ikke Car
+            Console.WriteLine("\n--- Alle ture ---");
+            foreach (var trip in myCar.GetTrips())
+            {
+                Console.WriteLine(trip.GetTripDetails());
+            }
+
+            Console.WriteLine($"\nOdometer: {myCar.Odometer} km");
         }
     }
 }
